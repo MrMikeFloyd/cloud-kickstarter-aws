15ed7814ff2bbd145936163cf56135f344f1ea15
Maik Fleuter <maik.fleuter@codecentric.de>
2021-12-28 20:10:01 +0100

    Replace Env vars with ecs exec role arn

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -92,6 +92,14 @@ output "source_repo_clone_url_http" {
   value = module.cicd.source_repo_clone_url_http
 }
 
+output "ecs_task_execution_role_arn_dev" {
+  value = module.compute-dev.ecs_task_execution_role_arn
+}
+
+output "ecs_task_execution_role_arn_prod" {
+  value = module.compute-prod.ecs_task_execution_role_arn
+}
+
 output "alb_address_dev" {
   value = module.network-dev.alb_address
 }
--- terraform/infrastructure/modules/compute-prod/outputs.tf
+++ terraform/infrastructure/modules/compute-prod/outputs.tf
@@ -13,3 +13,7 @@ output "codedeploy_app_name" {
 output "codedeploy_deployment_group_name" {
   value = aws_codedeploy_deployment_group.cloud-bootstrap-codedeploy-group.deployment_group_name
 }
+
+output "ecs_task_execution_role_arn" {
+  value = aws_ecs_task_definition.task-def.execution_role_arn
+}
